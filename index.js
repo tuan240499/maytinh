35e1f05d117ad0140d0c4749310f29ab01ac8609
tuan240499 <84010793+tuan240499@users.noreply.github.com>
2022-10-09 16:02:47 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var input = document.querySelector('.input');
+var iteam = Array.from(document.querySelectorAll('.iteam'));
+iteam.forEach(function(btn) {
+    btn.addEventListener('click', function() {
+
+        if (input.innerHTML == '0')
+            input.innerHTML = '';
+        if (btn.innerHTML == 'AC')
+            input.innerHTML = '0'
+        else if (btn.innerHTML == 'DEL') {
+            var arrtext = Array.from(input.innerHTML);
+            arrtext.splice(arrtext.length - 1, 1);
+            if (arrtext.length != 0)
+                input.innerHTML = arrtext.join('');
+            else input.innerHTML = '0';
+
+        } else if (btn.innerHTML == '=') {
+            input.innerHTML = eval(input.innerHTML);
+        } else input.innerHTML += btn.innerHTML;
+    })
+
+
+})
